473b56b7398092dc7f4a453a7773498259e1034b
aaronjamesreynolds <reynolaa@oregonstate.edu>
2018-04-23 14:18:07 -0700

    Added comments and removed if __name__ == "__main__":

--- NoQuD/tests/test_integration_step_and_read.py
+++ NoQuD/tests/test_integration_step_and_read.py
@@ -4,14 +4,13 @@ import os
 
 def test_integration():
 
+    """" This tests to see if data extracted from the input file can be used to create a StepCharacteristicSolver
+    object without error. """
+
     current_directory = os.path.abspath(__file__)
     file_path = os.path.join(current_directory, '../AI_test.csv')
 
-    sig_t, sig_sin, sig_sout, sig_f, nu, chi, groups, cells, cell_size, assembly_map, material, \
-    assembly_size, assembly_cells = read_csv.read_csv(file_path)
+    sig_t, sig_sin, sig_sout, sig_f, nu, chi, groups, cells, cell_size, assembly_map, material, assembly_size, \
+    assembly_cells = read_csv.read_csv(file_path)
 
     slab = StepCharacteristicSolver(sig_t, sig_sin, sig_sout, sig_f, nu, chi)
-
-if __name__ == '__main__':
-
-    test_integration()
