c21ffac0f75b8d63cc6d786ea32b5b64529b8036
aaronjamesreynolds <reynolaa@oregonstate.edu>
2018-04-28 14:05:34 -0700

    Define repeated coefficients.

--- NoQuD/nodal/Nodal.py
+++ NoQuD/nodal/Nodal.py
@@ -5,8 +5,17 @@ class Nodal:
 
     def __init__(self, diffusion_constant, sigma_r, cell_size, f, groups, n):
 
-        repeated_coefficients = np.zeros([6, 5])
+        repeated_coefficients = np.array([[0, 1, -3, 6, -10],
+                                 [0, 1, 3, 6, 10],
+                                 [0, 0, -12, 0, -40],
+                                 [0, 0, 0, -60, 0],
+                                 [0, 0, 0, 0, -140],
+                                 [0, 1, -3, 6, -10]])
+
         print repeated_coefficients
+
+
+
 
 if __name__ == "__main__":
 
