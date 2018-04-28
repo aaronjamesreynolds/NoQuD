5b36e4607246f5d979eef061229a5e6cc6080905
aaronjamesreynolds <reynolaa@oregonstate.edu>
2018-04-28 13:56:54 -0700

    Started Nodal module for solving the homogeneous equation.

---
+++ NoQuD/nodal/Nodal.py
@@ -0,0 +1,13 @@
+
+import numpy as np
+
+class Nodal:
+
+    def __init__(self, diffusion_constant, sigma_r, cell_size, f, groups, n):
+
+        repeated_coefficients = np.zeros([6, 5])
+        print repeated_coefficients
+
+if __name__ == "__main__":
+
+    test = Nodal(1, 1, 1, 1, 1, 1)
