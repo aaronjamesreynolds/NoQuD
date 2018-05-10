635de8dffa4bfcb87e42f73b33e7c1874f5445fe
aaronjamesreynolds <reynolaa@oregonstate.edu>
2018-05-09 17:01:35 -0700

    Placed test files into a separate folder to clean things up.

--- NoQuD/tests/test_integration_step_and_read.py
+++ NoQuD/tests/test_integration_step_and_read.py
@@ -10,7 +10,7 @@ def test_integration():
     object without error. """
 
     current_directory = os.path.dirname(os.path.realpath(__file__))
-    file_path = os.path.join(current_directory, 'AI_test.csv')
+    file_path = os.path.join(current_directory, 'testing_files/AI_test.csv')
 
     sig_t, sig_sin, sig_sout, sig_f, nu, chi, groups, cells, cell_size, assembly_map, material, assembly_size, \
     assembly_cells = read_csv.read_csv(file_path)
@@ -21,7 +21,7 @@ def test_integration():
 def test_step_characteristic_solve():
 
     current_directory = os.path.dirname(os.path.realpath(__file__))
-    file_path = os.path.join(current_directory, 'AI_test.csv')
+    file_path = os.path.join(current_directory, 'testing_files/AI_test.csv')
 
     sig_t, sig_sin, sig_sout, sig_f, nu, chi, groups, cells, cell_size, assembly_map, material, assembly_size, \
     assembly_cells = read_csv.read_csv(file_path)
@@ -32,7 +32,7 @@ def test_step_characteristic_solve():
     rows = []  # initialize a temporary storage variable
     elements = []  # initialize a temporary storage variable
 
-    file_path = os.path.join(current_directory, 'validation_flux.csv')
+    file_path = os.path.join(current_directory, 'testing_files/validation_flux.csv')
 
     # Read in flux validation data.
     with open(file_path, 'r') as file:
@@ -48,7 +48,7 @@ def test_step_characteristic_solve():
     # Reshape matrix to correct format.
     flux_ref = numpy.reshape(elements, [number_rows, number_elements / number_rows]).astype(numpy.float)
 
-    file_path = os.path.join(current_directory, 'validation_k.csv')
+    file_path = os.path.join(current_directory, 'testing_files/validation_k.csv')
 
     # Read in eigenvalue validation data.
     with open(file_path, 'r') as file:
@@ -73,7 +73,7 @@ def test_more_than_two_assemblies():
     StepCharacteristicSolver object without error. """
 
     current_directory = os.path.dirname(os.path.realpath(__file__))
-    file_path = os.path.join(current_directory, 'AI_3plus_test.csv')
+    file_path = os.path.join(current_directory, 'testing_files/AI_3plus_test.csv')
 
     sig_t, sig_sin, sig_sout, sig_f, nu, chi, groups, cells, cell_size, assembly_map, material, assembly_size, \
     assembly_cells = read_csv.read_csv(file_path)
@@ -91,7 +91,7 @@ def test_more_than_two_assemblies():
 #     plt.show()
 #     print "Multiplication Factor: {0}".format(slab.k_new)
 #
-# if __name__ =="__main__":
-#     test_integration()
-#     test_step_characteristic_solve()
-#     test_more_than_two_assemblies()
+if __name__ =="__main__":
+    test_integration()
+    test_step_characteristic_solve()
+    test_more_than_two_assemblies()
