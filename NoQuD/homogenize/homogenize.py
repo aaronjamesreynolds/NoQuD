a3c0ce50dd77bfef7f41748efeea4f0638e99d33
aaronjamesreynolds <reynolaa@oregonstate.edu>
2018-05-14 15:46:45 -0700

    Started building HomogenizeGlobal class. Wrote __init__ method.

--- NoQuD/homogenize/homogenize.py
+++ NoQuD/homogenize/homogenize.py
@@ -3,7 +3,8 @@ from NoQuD.read_input_data import read_csv_input_file as read_csv
 from NoQuD.step_characteristic_solve.StepCharacteristicSolver import *
 import os
 
-class Homogenize():
+
+class HomogenizeAssembly:
 
     def __init__(self, filename):
 
@@ -57,7 +58,44 @@ class Homogenize():
         self.calculate_homogenized_nuclear_data()
         self.calculate_discontinuity_factors()
 
+
+class HomogenizeGlobe:
+
+    def __init__(self, single_assembly_input_files):
+        # single_assembly_input_files should contain the assembly information file, followed by the assembly information
+        # files for each unique assembly.
+        self.single_assembly_input_files = single_assembly_input_files
+
+        current_directory = os.path.dirname(os.path.realpath(__file__))
+        file_path = os.path.join(current_directory, self.single_assembly_input_files[0])
+
+        self.sig_t, self.sig_sin, self.sig_sout, self.sig_f, self.nu, self.chi, self.groups, self.cells, self.cell_size \
+            , self.assembly_map, self.material, self.assembly_size, self.assembly_cells = read_csv.read_csv(file_path)
+
+        # 'a' stands for assembly. The variables below will hold the homogenized nuclear data for each unique assembly.
+        self.eddington_factor_a = np.zeros((self.groups, len(single_assembly_input_files)-1))
+        self.sig_r_a = np.zeros((self.groups, len(single_assembly_input_files)-1))
+        self.sig_t_a = np.zeros((self.groups, len(single_assembly_input_files)-1))
+        self.sig_sin_a = np.zeros((self.groups, len(single_assembly_input_files)-1))
+        self.sig_sout_a = np.zeros((self.groups, len(single_assembly_input_files)-1))
+        self.sig_f_a = np.zeros((self.groups, len(single_assembly_input_files)-1))
+        self.f_a = np.zeros((self.groups, len(single_assembly_input_files)-1))
+
+        # 'g' stand for global. The variables below will hold the homogenized nuclear data for each node.
+        self.eddington_factor_g = np.zeros((self.groups, len(self.assembly_map)))
+        self.sig_r_g = np.zeros((self.groups, len(self.assembly_map)))
+        self.sig_t_g = np.zeros((self.groups, len(self.assembly_map)))
+        self.sig_sin_g = np.zeros((self.groups, len(self.assembly_map)))
+        self.sig_sout_g = np.zeros((self.groups, len(self.assembly_map)))
+        self.sig_f_g = np.zeros((self.groups, len(self.assembly_map)))
+        self.f_g = np.zeros((self.groups, len(self.assembly_map)))
+
+    # initialize matrices for global map based on dimension of single_assembly_input_files
+
+
+
+
 if __name__ == '__main__':
-    test = Homogenize('assembly_info_single_test.csv')
+    test = HomogenizeAssembly('assembly_info_single_test.csv')
     test.perform_homogenization()
     print test.sig_sin_h
