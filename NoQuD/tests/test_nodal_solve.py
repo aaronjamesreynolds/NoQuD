0033e3bf7bafb9a512d503272b9357d19c280c39
aaronjamesreynolds <reynolaa@oregonstate.edu>
2018-05-27 18:35:17 -0700

    Made a NodalSolve class test

---
+++ NoQuD/tests/test_nodal_solve.py
@@ -0,0 +1,19 @@
+import NoQuD.HomogeneousSolve.NodalSolve as NS
+import os
+
+
+def test_nodal_solve_running():
+
+    """ Make sure the NodalSolve class can be initialized without error."""
+
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    local_path1 = os.path.join('testing_files', 'assembly_info_test.csv')
+    local_path2 = os.path.join('testing_files', 'assembly_info_single_test.csv')
+    file_path1 = os.path.join(current_dir, local_path1)
+    file_path2 = os.path.join(current_dir, local_path2)
+
+    test = NS.NodalSolve([file_path1, file_path2, file_path2])
+    test.solve()
+
+if __name__=="__main__":
+    test_nodal_solve_running()
